ac5cd164dc8f8bda46157ff2ef1c8f8b9186a4d8
nikhil-neu <nikhil.rajan67@gmail.com>
2016-03-26 01:55:47 +0530

    First Check in

---
+++ Build/default.ps1
@@ -0,0 +1,58 @@
+﻿properties {
+	$testMessage = 'Executed Test!'
+	$compileMessage = 'Executed Compile!'
+	$cleanMessage = 'Executed Clean'
+
+	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
+	$outputDirectory = "$solutionDirectory\.build"
+	$temporaryOutputDirectory = "$outputDirectory\temp"
+	$buildConfiguration ="Release"
+	$buildPlatform = "Any CPU"
+}
+FormatTaskName "`r`n`r`n ----------------Executing {0} Task ---------- "
+
+task default -depends Test
+
+task Init -description "Initializes the build by removing the previous artifacts
+and creating output directories" -requiredVariables outputDirectory,temporaryOutputDirectory {
+	Assert ("Debug","Release" -contains $buildConfiguration)`
+	"Invalid build configuration '$buildConfiguration' . Valid values are 'Debug' or 'Release'"
+
+	Assert ("x86","x64","Any CPU" -contains $buildPlatform )`
+	"Invalid build platform '$buildPlatform'. Valid values are 'x86' , 'x64' or 'Any CPU' "
+	# Remove previous byuild results
+	if (Test-Path $outputDirectory)
+	{
+		Write-Host "Removing O/P directory located at $outputDirectory"
+		Remove-Item $outputDirectory -Force -Recurse
+	}
+
+	Write-Host "Creating output directory located at ..\.build"
+	New-Item $outputDirectory -ItemType Directory | Out-Null
+
+	Write-Host "Creating Temperory directory located
+		at $temporaryOutputDirectory"
+	New-Item $temporaryOutputDirectory -ItemType Directory | Out-Null
+}
+
+
+task Clean  -description "Removes temperory code" {
+	Write-Host $cleanMessage
+	
+}
+ 
+task Compile `
+     -depends Init `
+	 -description "Compliles the code" `
+	 -requiredVariables solutionFile,buildConfiguration,buildPlatform,temporaryOutputDirectory `
+	 {
+	 Write-Host "Building soltion $solutionFile"
+	 Exec{
+		 msbuild $solutionFile "/p:OutDir=$temporaryOutputDirectory"
+		 }
+	 Write-Host "Ms build completed"		 
+
+	}
+task Test -depends Compile, Clean -description "Executes the test" {
+	Write-Host $testMessage
+}
---
+++ build.ps1
@@ -0,0 +1,22 @@
+﻿cls
+
+Remove-Module [p]sake
+
+# find psake's path
+$psakeModule = (Get-ChildItem (".\packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -last 1
+
+Import-Module $psakeModule
+
+Invoke-psake     -buildFile .\Build\default.ps1 `
+				 -taskList Test `
+				 -framework 4.5.2 `
+				 -properties @{
+					 "buildConfiguration"="Release"
+					 "buildPlatform" = "Any CPU"} `
+				 -parameters @{
+					 "solutionFile" = "..\FabrikamFiber.CallCenter.sln"}
+
+ Write-Host "Build exit code:" $LASTEXITCODE
+
+ # Propagating the exit code so that the build actually fails when there is a problem
+ exit $LASTEXITCODE
